3fe53fdf07fcaf8c9913a2642fba25797d0f3007
GerardoKS <143908957+GerardoKS@users.noreply.github.com>
2024-10-24 23:03:57 -0600

    Update creditTest.java

--- creditTest.java
+++ creditTest.java
@@ -37,60 +37,61 @@ public class creditTest {
      * @return returns wether the test passed(true) or failed(false)
      */
     private boolean testChangeBalance(){
+        Files testFile = new Files();
         testCreditObject.setBalance(0);
         //try a condition that should always fail
-        if(testCreditObject.changeBalance(Double.NEGATIVE_INFINITY)){
+        if(testCreditObject.changeBalance(Double.NEGATIVE_INFINITY, testFile)){
             System.out.println("failed amount neg inf change balance");
             return false;
         }    
         //try 0
-        if(!testCreditObject.changeBalance(0)){
+        if(!testCreditObject.changeBalance(0, testFile)){
             System.out.println("failed amount 0 change balance");
             return false;
         }    
         //try valeus with decimals
-        if(!testCreditObject.changeBalance(-0.5)){
+        if(!testCreditObject.changeBalance(-0.5, testFile)){
             System.out.println("failed amount -0.5 change balance");
             return false;
         }    
         //pay more that you owe test for values with decimals
         testCreditObject.setBalance(0);
-        if(testCreditObject.changeBalance(0.5)){
+        if(testCreditObject.changeBalance(0.5, testFile)){
             System.out.println("failed amount 0.5 change balance");
             return false;
         }    
         //try positive values
-        if(!testCreditObject.changeBalance(-1)){
+        if(!testCreditObject.changeBalance(-1, testFile)){
             System.out.println("failed amount -1 change balance");
             return false;
         }    
         //pay more than you owe test for no decimal numbers
         testCreditObject.setBalance(0);
-        if(testCreditObject.changeBalance(1)){
+        if(testCreditObject.changeBalance(1, testFile)){
             System.out.println("failed amount 1 change balance");
             return false;
         }    
         //if they pay off all their debt
         testCreditObject.setBalance(-1);
-        if(!testCreditObject.changeBalance(1)){
+        if(!testCreditObject.changeBalance(1, testFile)){
             System.out.println("failed amount 1 balance -1 change balance");
             return false;
         }
         //exceed max
         testCreditObject.setBalance(-1);
-        if(testCreditObject.changeBalance(-2)){
+        if(testCreditObject.changeBalance(-2, testFile)){
             System.out.println("failed amount -2 balance -1 change balance");
             return false;
         }  
         //reach max
         testCreditObject.setBalance(-1.5);
-        if(!testCreditObject.changeBalance(-0.5)){
+        if(!testCreditObject.changeBalance(-0.5, testFile)){
             System.out.println("failed amount -0.5 balance -1.5 change balance");
             return false;
         }  
         //exceed max with decimal value
         testCreditObject.setBalance(-2);
-        if(testCreditObject.changeBalance(-0.5)){
+        if(testCreditObject.changeBalance(-0.5, testFile)){
             System.out.println("failed amount -0.5 balance -2 change balance");
             return false;
         }  
