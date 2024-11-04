4c739f872828a9a60f684593418d439e4e9e4e0b
g s <gerardoks2004@gmail.com>
2024-11-03 21:09:45 -0700

    fixed the tests now test suite works

--- tst/TestSuite.java
+++ tst/TestSuite.java
@@ -1,12 +1,13 @@
 package tst;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
 
-@Suite
-@SelectClasses({
-    accountTest.class,
-    creditTest.class
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    AccountTest.class,
+    CreditTest.class
 })
-public class TestSuite{
+public class TestSuite {
 }
 
