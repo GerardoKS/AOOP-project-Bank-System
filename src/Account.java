3125a8f89dccb0a46774ed17d437d4c864bfe10d
g s <gerardoks2004@gmail.com>
2024-11-30 23:22:00 -0700

    lock feature added

--- src/Account.java
+++ src/Account.java
@@ -14,6 +14,10 @@ public class Account{
     protected double balance;
     protected double startingBalance;
     protected Customer accountHolder;
+    /**
+     * lock is a new feature to lock your account
+     */
+    protected boolean lock = false;
 
  
     public Account(){}
@@ -51,6 +55,14 @@ public class Account{
         return accountHolder;
     }
  
+    public void lockAccount(){
+        lock = true;
+    }
+
+    public void unlockAccount(){
+        lock = false;
+    }
+
     /**
      * Changes the balance based on the amount given
      * @param amount is positive if it is for a deposit or negative if withdrawl
@@ -99,7 +111,7 @@ public class Account{
      */
     public boolean canWithdraw(double amount){
         double checkIfValidBalance = balance + amount;
-        if(checkIfValidBalance < 0){
+        if(checkIfValidBalance < 0 || lock){
             return false;
         }
         return true;
@@ -108,6 +120,9 @@ public class Account{
      * The Deposit should always be true for this method since you dont have a max amount of money you can have
      */
     public boolean canDeposit(double amount){
+        if(lock){
+            return false;
+        }
         return true;
     }
 
